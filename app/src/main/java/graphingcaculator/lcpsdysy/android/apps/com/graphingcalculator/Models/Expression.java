4d9e4f1f973ef1618bccd935f3098a85da0ce023
PrasannS <singhal.prasann07@gmail.com>
2018-10-20 18:52:31 -0500

    The basic back and front end code is all set up for the numerical calculator. Additional functions need to be added.

---
+++ app/src/main/java/graphingcaculator/lcpsdysy/android/apps/com/graphingcalculator/Models/Expression.java
@@ -0,0 +1,66 @@
+package graphingcaculator.lcpsdysy.android.apps.com.graphingcalculator.Models;
+
+import java.util.ArrayList;
+
+public class Expression {
+    private double solution;
+    public char[] bedmas = {'/','*','+','-'};
+    private ArrayList<Double>i ;
+    ArrayList<Character>c ;
+    public Expression(ArrayList<Character>ct,ArrayList<Double>it){
+        i = it;
+        c= ct;
+        int ref = c.size();
+        int ind;
+        char symbol;
+        while(ref>=1){
+            ind = getInd();
+            symbol = c.get(ind);
+            i.add(ind,evaluate(i.get(ind),i.get(ind+1),symbol));
+            i.remove(ind+1);
+            i.remove(ind+1);
+            c.remove(ind);
+            ref--;
+        }
+        solution = i.get(0);
+    }
+
+    public double getSolution(){
+        return solution;
+    }
+
+    public double evaluate(double a, double b, char c){
+        switch (c){
+            case '*':
+                return a*b;
+            case '/':
+                return a/b;
+            case '+':
+                return a+b;
+            case '-':
+                return a-b;
+        }
+        return a;
+    }
+
+    public int getInd(){
+        boolean ready = false;
+        int ans = 100;
+        int ind = 0;
+        char current ='/';
+        while(ind<=3&&!ready){
+            if(ind<=3)
+            current = bedmas[ind];
+            for(int i = 0; i<c.size(); i++){
+                if(c.get(i)==current){
+                    ans = i;
+                    ready = true;
+                    break;
+                }
+            }
+            ind++;
+        }
+        return ans;
+
+    }
+}
