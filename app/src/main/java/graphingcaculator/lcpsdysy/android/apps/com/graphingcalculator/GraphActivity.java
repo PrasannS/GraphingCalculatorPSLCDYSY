3b2bf631453a69c85ffaf90d7b928786363ea617
YuanSamuel <samuel6161@gmail.com>
2018-11-01 21:30:51 -0500

    Updated a couple of stuff, super minor

--- app/src/main/java/graphingcaculator/lcpsdysy/android/apps/com/graphingcalculator/GraphActivity.java
+++ app/src/main/java/graphingcaculator/lcpsdysy/android/apps/com/graphingcalculator/GraphActivity.java
@@ -15,10 +15,6 @@ import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
-import org.w3c.dom.Text;
-
-import java.util.StringTokenizer;
-
  public class GraphActivity extends AppCompatActivity {
     private ImageButton home;
     private ImageButton settings;
